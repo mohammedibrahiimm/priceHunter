58ca366aed43f857a158eed496ddb2f1a33b1cc6
3moJunioR <m.wa7ba@gmail.com>
2025-04-20 14:16:51 +0200

    shein google search2

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ async def predict_price(item: Item):
     }
 
     if state == "new":
-       product_urls["shein_search_link"] = shein_link
+       product_urls["shein_search_link"] = f"https://www.google.com/search?q=site:shein.com+{query_encoded}"
     elif state == "used":
         product_urls["ebay_search_link"] = ebay_link
 
