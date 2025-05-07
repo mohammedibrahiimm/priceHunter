dd66069acb6dc68a73240b1374d1b4a95ea0bc4c
3moJunioR <m.wa7ba@gmail.com>
2025-05-08 02:06:12 +0300

    OfficialStores3

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ encoders = joblib.load(ENCODERS_PATH)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5122"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
