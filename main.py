1229c7e6f78214fdabeeb50276ac48b42bd85fd4
3moJunioR <m.wa7ba@gmail.com>
2025-05-08 02:20:15 +0300

    OfficialStores5

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def get_lowest_price_link(query: str):
         "search_engine": "google.com",
         "tbm": "shop",
         "num": 5,
-        "apikey": "2376c9b0-1dd4-11f0-a27c-4da665a28829"
+        "apikey": "a24448b0-2b99-11f0-a982-df2d75737eeb"
     }
 
     try:
