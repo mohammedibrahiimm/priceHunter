4b7877dddd9da05250e281ab4f24cc9da4e0d36b
mohammedibrahiimm <mohamedalii7405@gmail.com>
2025-06-14 19:12:22 +0300

    Update main.py
    api-key added

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def get_lowest_price_link(query: str):
         "search_engine": "google.com",
         "tbm": "shop",
         "num": 5,
-        "apikey": "890272d0-2b9a-11f0-9921-bdbceb4bbdcd"
+        "apikey": "2376c9b0-1dd4-11f0-a27c-4da665a28829"
     }
 
     try:
